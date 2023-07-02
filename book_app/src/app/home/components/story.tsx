29fa9f7a7a6cbede6c18465e97429029a157c17f
Fa_HD <79041084+jokerf6@users.noreply.github.com>
2023-07-02 11:57:20 +0300

    Update story.tsx

--- book_app/src/app/home/components/story.tsx
+++ book_app/src/app/home/components/story.tsx
@@ -2,12 +2,7 @@ export default function Story() {
   return (
     <div className=" border">
       <p className=" text-gray-800 p-4 border-2 border-black">
-        In Egyptian mythology, Sekhmet was believed to unleash her wrath upon
-        humanity as a punishment for their disobedience. Her ferocity was said
-        to be so great that it took the intervention of the god Thoth to appease
-        her and prevent total destruction. Despite her destructive aspects,
-        Sekhmet was also associated with healing and was believed to possess the
-        power to cure illnesses and ward off evil spirits.
+        Seshat, in ancient Egyptian religion, the goddess of writing and measurement and the ruler of books. She was the consort of the god Djhuty (Thoth), and both were divine scribes (sesb). She was portrayed as a female wearing a headband with horns and a star with her name written on it. Representations of her typically show her dress to be a plain sheath covered by a long panther skin, with the tail reaching her feet. She was often depicted with the notched palm rib that represented the passing of time.
       </p>
     </div>
   );
